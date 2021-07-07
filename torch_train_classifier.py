c2bac4e9b475bae77394204437a7677caa0638c1
prithvi-poddar <prithvi.poddar@gmail.com>
2021-07-07 08:09:47 +0530

    minor changes

--- torch_train_classifier.py
+++ torch_train_classifier.py
@@ -14,19 +14,23 @@ import torch.utils.data
 import torch_network
 import utils
 
+
 def feature_transform_regularizer(trans):
     d = trans.size()[1]
     batchsize = trans.size()[0]
     I = torch.eye(d)[None, :, :]
     if trans.is_cuda:
         I = I.cuda()
-    loss = torch.mean(torch.norm(torch.bmm(trans, trans.transpose(2,1)) - I, dim=(1,2)))
+    loss = torch.mean(torch.norm(
+        torch.bmm(trans, trans.transpose(2, 1)) - I, dim=(1, 2)))
     return loss
+
 
 DATA_DIR = "ModelNet10"     # <- Set this path correctly
 
 num_points_per_cloud = 4096     # <- you can modify this number as needed
-train_pc, test_pc, train_labels, test_labels, class_ids = utils.create_point_cloud_dataset(DATA_DIR, num_points_per_cloud)
+train_pc, test_pc, train_labels, test_labels, class_ids = utils.create_point_cloud_dataset(
+    DATA_DIR, num_points_per_cloud)
 
 # once loaded save the numpy arrays to pickle files to use later
 pickle.dump(train_pc, open("trainpc.pkl", "wb"))
@@ -41,4 +45,3 @@ pickle.dump(class_ids, open("class_ids.pkl", "wb"))
 # test_pc = pickle.load(open("testpc.pkl", "rb"))
 # test_labels = pickle.load(open("testlabels.pkl", "rb"))
 # class_ids = pickle.load(open("class_ids.pkl", "rb"))
-
