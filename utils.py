dc83ec3a282c42ca93de2b381d2dd5e54eace3cc
jhd-zju <37507525+jhd-zju@users.noreply.github.com>
2021-07-08 22:51:42 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import trimesh
 import trimesh.sample
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from trimesh.triangles import bounds_tree
 import tensorflow as tf
 import pickle
@@ -152,12 +153,12 @@ def visualize_cloud(point_cloud, true_label='', predicted_label=''):
     """
     if true_label=='':
         fig = plt.figure()
-        ax = fig.add_subplot(111, projection="3d")
+        ax = Axes3D(fig)
         ax.scatter(point_cloud[:, 0], point_cloud[:, 1], point_cloud[:, 2])
         plt.show()
     else:
         fig = plt.figure()
-        ax = fig.add_subplot(111, projection="3d")
+        ax = Axes3D(fig)
         ax.scatter(point_cloud[:, 0], point_cloud[:, 1], point_cloud[:, 2])
         ax.set_xlim(-1,1)
         ax.set_ylim(-1,1)
