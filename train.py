45a1c46cd776c4a8e47328ddf217132ff126debd
prithvi-poddar <prithvi.poddar@gail.com>
2021-07-08 14:13:58 +0530

    reduced network

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import numpy as np
 import tensorflow as tf
 import trimesh.sample
 from tensorflow import keras
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
 from tensorflow.keras import layers
 from matplotlib import pyplot as plt
 import pickle
@@ -10,6 +12,7 @@ import tensorboard
 from datetime import datetime
 
 import network
+# import reduced_network
 import utils
 import provider
 
@@ -21,29 +24,29 @@ for gpu in gpus:
 DATA_DIR = "ModelNet10/"     # <- Set this path correctly
 
 
-num_points_per_cloud = 4096
-# train_pc, test_pc, train_labels, test_labels, class_ids = utils.create_point_cloud_dataset(DATA_DIR, num_points_per_cloud)
+num_points_per_cloud = 1024
+train_pc, test_pc, train_labels, test_labels, class_ids = utils.create_point_cloud_dataset(DATA_DIR, num_points_per_cloud)
 
 # print(train_pc.shape)
-# train_pc = provider.normalize_pc(train_pc)
+train_pc = provider.normalize_pc(train_pc)
 # print(train_pc.shape)
-# test_pc = provider.normalize_pc(test_pc)
+test_pc = provider.normalize_pc(test_pc)
 
-# pickle.dump(train_pc, open("trainpc.pkl", "wb"))
-# pickle.dump(test_pc, open("testpc.pkl", "wb"))
-# pickle.dump(train_labels, open("trainlabels.pkl", "wb"))
-# pickle.dump(test_labels, open("testlabels.pkl", "wb"))
-# pickle.dump(class_ids, open("class_ids.pkl", "wb"))
+pickle.dump(train_pc, open("trainpc.pkl", "wb"))
+pickle.dump(test_pc, open("testpc.pkl", "wb"))
+pickle.dump(train_labels, open("trainlabels.pkl", "wb"))
+pickle.dump(test_labels, open("testlabels.pkl", "wb"))
+pickle.dump(class_ids, open("class_ids.pkl", "wb"))
 
 # utils.visualize_cloud(train_pc[0])
 
 
 # # load the data from pickle files if already present
-train_pc = pickle.load(open("trainpc.pkl", "rb"))
-train_labels = pickle.load(open("trainlabels.pkl", "rb"))
-test_pc = pickle.load(open("testpc.pkl", "rb"))
-test_labels = pickle.load(open("testlabels.pkl", "rb"))
-class_ids = pickle.load(open("class_ids.pkl", "rb"))
+# train_pc = pickle.load(open("trainpc.pkl", "rb"))
+# train_labels = pickle.load(open("trainlabels.pkl", "rb"))
+# test_pc = pickle.load(open("testpc.pkl", "rb"))
+# test_labels = pickle.load(open("testlabels.pkl", "rb"))
+# class_ids = pickle.load(open("class_ids.pkl", "rb"))
 
 train_pc = provider.rotate_point_cloud(train_pc)
 # test_pc = provider.rotate_point_cloud(test_pc)
@@ -66,18 +69,22 @@ outputs = network.pointnet_classifier(inputs, num_classes=10)
 model = keras.Model(inputs=inputs, outputs=outputs, name="pointnet")
 model.summary()
 
-logdir = "logs/classifier/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+logdir = "logs/classifier_2/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
+
+es = EarlyStopping(monitor='val_accuracy', mode='max', patience=20)
+mc = ModelCheckpoint('classifier_model_2_best.h5', monitor='val_accuracy', mode='max', save_best_only=True)
+
 
 # 2. Set the loss function, optimizer and metrics to print
 model.compile(
     loss=keras.losses.CategoricalCrossentropy(),     # <- choose a suitable loss function
-    optimizer=keras.optimizers.Adam(learning_rate=0.00001),      # <- you may modify this if you like
+    optimizer=keras.optimizers.Adam(learning_rate=0.00001, beta_1=0.9, beta_2=0.999),      # <- you may modify this if you like
     metrics=["accuracy"],    # <- choose a suitable metric, https://www.tensorflow.org/api_docs/python/tf/keras/metrics
 )
 
 # train the network
 num_epochs = 500      # <- change this value as needed
-model.fit(train_dataset, epochs=num_epochs, validation_data=test_dataset, callbacks=[tensorboard_callback])
+model.fit(train_dataset, epochs=num_epochs, validation_data=test_dataset, callbacks=[tensorboard_callback, es, mc])
 
-model.save('classifier_model')
+model.save('classifier_model_2')
